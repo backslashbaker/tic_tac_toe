58b41ded0a6d1b7341976d16a6cb70b975ab4edc
Derek Baker <derek.baker89@gmail.com>
2020-09-28 10:22:12 +0100

    pass take turn test on game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,6 +10,14 @@ describe Board do
 
             expect(game.welcome_message).to eq 'Welcome to Tic Tac Toe'
         end
+    end
 
+    context "new game" do
+        it "it displays a welcome message to users" do
+            game = Game.new
+            game.take_turn(0, 0, game.player_one)
+
+            expect(game.board.grid[0][0]).to eq("O")
+        end
     end
 end
