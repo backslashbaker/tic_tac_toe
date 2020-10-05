1e07dd7105b772618dc7d1925d861e67c85b0f7a
Derek Baker <derek.baker89@gmail.com>
2020-10-05 15:17:21 +0100

    pass display test and remove input test

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -4,82 +4,59 @@ require_relative '../lib/game'
 
 describe Display do
 
-    let(:console) { StringIO.new }
-    let(:input) { StringIO.new }
-
     context "welcome message " do
         it "displays a welcome message to users" do
-            display = Display.new(input, console)
+            display = Display.new
 
             display.welcome_message
 
-            expect(console.string).to eq("Welcome to Tic Tac Toe\n\n")
+            expect{ display.welcome_message }.to output("Welcome to Tic Tac Toe\n\n").to_stdout
         end
     end
 
     context "displays game board" do
         it "displays a blank board to users" do
-            display = Display.new(input, console)
+            display = Display.new
             grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
             expected_grid = " 1 | 2 | 3 \n------------\n 4 | 5 | 6 \n------------\n 7 | 8 | 9 \n\n"
-
-            display.display_board(grid)
-
-            expect(console.string).to eq(expected_grid)
-        end
-    end
-
-    context "gets user input" do
-        it "asks user to choose a coordinate" do
-            display = Display.new(input, console)
-
-            display.get_input
-
-            expect(input).to receive(:gets).and_return('5')
-
-            display.get_input
+            
+            expect { display.display_board(grid) }.to output(expected_grid).to_stdout
         end
     end
 
     context "user prompt " do
         it "displays a prompt for user to choose a place to mark on board" do
-            display = Display.new(input, console)
+            display = Display.new
 
             display.request_index
 
-            expect(console.string).to eq("Please choose a number between 1 - 9: ")
+            expect { display.request_index }.to output("Please choose a number between 1 - 9: ").to_stdout
         end
     end
 
     context "error handling" do
         it "displays an error message when user enters incorrect index" do
-            display = Display.new(input, console)
+            display = Display.new
 
-            display.error_message
-
-            expect(console.string).to eq("Input error. Try again.\n")
+            expect { display.error_message }.to output("Input error. Try again.\n").to_stdout
         end
     end
 
     context "when X wins the game" do
         it "displays a win message" do
-            display = Display.new(input, console)
+            display = Display.new
             player_two = Player.new("X")
 
-            display.game_result_message(player_two)
-
-            expect(console.string).to eq("X wins!")
+            expect { display.game_result_message(player_two) }.to output("X wins!").to_stdout
         end
     end
 
     context "when O wins the game" do
         it "displays a win message" do
-            display = Display.new(input, console)
+            display = Display.new
             player_one = Player.new("O")
 
-            display.game_result_message(player_one)
-
-            expect(console.string).to eq("O wins!")
+            expect { display.game_result_message(player_one) }.to output("O wins!").to_stdout
         end
     end
        
