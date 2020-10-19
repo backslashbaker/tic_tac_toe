9eb98d3b63d36db833594abb188fd0067e6c920e
joannafawl <joanna.fawl@madetech.com>
2020-10-19 14:44:19 +0100

    Deleted unnecessary test

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,13 +1,15 @@
 require_relative '../lib/computer'
 
 describe Computer do
-    context "when there is one available space" do
-        it "plays into that space" do
-            computer = Computer.new
-            board = double("Board")
-            allow(board).to receive(:grid).and_return(["O", "X", "X", "X", "O", "O", "X", "O", 9])
+    # context "when we call the #move function" do
+    #     it "updates the grid" do
+    #         computer = Computer.new
+    #         board = double("Board")
+    #         allow(board).to receive(:grid).and_return(["O", "X", "X", "X", "O", "O", "X", "O", 9])
+    #         allow(board).to receive(:update_grid).and_return(["O", "X", "X", "X", "O", "O", "X", "O", "X"])
+    #         computer.move(8, board)
 
-            expect(computer.move(board)).to eq(8)
-        end
-    end
+    #         expect(board.grid).to eq(["O", "X", "X", "X", "O", "O", "X", "O", "X"])
+    #     end
+    # end
 end
