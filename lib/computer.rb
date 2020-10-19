3296328b8205d7110d21e67a3d109483db8992ac
joannafawl <joanna.fawl@madetech.com>
2020-10-19 10:35:22 +0100

    Pass test for comp to play in first empty space

--- lib/computer.rb
+++ lib/computer.rb
@@ -7,17 +7,21 @@ class Computer < Player
     end
 
     def move(board)
-        index = 0
-        while !found do
-            if minimax.score(board, empty_spaces[index], @marker) == 1
-                found = true
+        board.grid.each_with_index { |space, index|
+            if space.is_a?(Integer)
+                return index
             end
-            index += 1
-        end
-        empty_spaces.each do |space|
+        }
+        # index = 0
+        # while !found do
+        #     if minimax.score(board, empty_spaces[index], @marker) == 1
+        #         found = true
+        #     end
+        #     index += 1
+        # end
+        # empty_spaces.each do |space|
             
-        end
-        return 8
+        # end
     end
 
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,8 +2,8 @@ require_relative '../lib/computer'
 
 describe Computer do
     context "when there is one available space" do
-        xit "plays into that space" do
-            computer = Computer.new("X")
+        it "plays into that space" do
+            computer = Computer.new
             board = double("Board")
             allow(board).to receive(:grid).and_return(["O", "X", "X", "X", "O", "O", "X", "O", 9])
 
