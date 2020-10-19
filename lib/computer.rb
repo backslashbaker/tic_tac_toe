70bb78ccb3301dbb0d89335e2b41962fd49ff30b
joannafawl <joanna.fawl@madetech.com>
2020-10-19 12:03:11 +0100

    Refactored Computer move function to call minimax

--- lib/computer.rb
+++ lib/computer.rb
@@ -4,24 +4,11 @@ class Computer < Player
 
     def initialize
         @marker = "X"
+        @minimax = Minimax.new
     end
 
     def move(board)
-        board.grid.each_with_index { |space, index|
-            if space.is_a?(Integer)
-                return index
-            end
-        }
-        # index = 0
-        # while !found do
-        #     if minimax.score(board, empty_spaces[index], @marker) == 1
-        #         found = true
-        #     end
-        #     index += 1
-        # end
-        # empty_spaces.each do |space|
-            
-        # end
+        @minimax.minimax(board, @marker)
     end
 
 end
